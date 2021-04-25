9259cf2f571a298c1d79a9920e2feec5e88be93d
felkholy <felkholy@egeos.de>
2021-04-25 11:57:31 +0200

    refactor: impl createCustomerAccount of customer service

--- internal/service/customer/service.go
+++ internal/service/customer/service.go
@@ -1,1 +1,23 @@
 package customer
+
+import (
+	"github.com/faroukelkholy/bank/internal/service/models"
+	"github.com/faroukelkholy/bank/internal/storage"
+)
+
+type Service interface {
+	CreateCustomerAccount(id string, account *models.Account) error
+}
+
+type service struct {
+	repo storage.CustomerRepository
+}
+
+func New(repo storage.CustomerRepository) Service {
+	return &service{repo: repo}
+}
+
+func (s *service) CreateCustomerAccount(id string, account *models.Account) error {
+	return s.repo.CreateCustomerAccount(id, account)
+}
+
